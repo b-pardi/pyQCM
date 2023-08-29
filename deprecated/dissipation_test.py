0aa89282d827dedc2ca854923c5b6e772429d47f
brandonator24 <72064522+brandonator24@users.noreply.github.com>
2023-08-28 22:18:30 -0700

    more test scripts for qcmi fwhm conversion problem

--- deprecated/dissipation_test.py
+++ deprecated/dissipation_test.py
@@ -8,12 +8,17 @@ print(qcmi_df, '\n', qsense_df)
 qsense_offset_df = pd.read_excel("../offset_data/QSense_BSA-offset.xlsx", index_col=None)
 qcmi_offset_df = pd.read_excel("../offset_data/QCMI-BSA-offset.xlsx", index_col=None)
 
-# only want dissipation
-qsense_offset_df = qsense_offset_df.filter(like='D_')
-qcmi_offset_df = qcmi_offset_df.filter(like='D_')
+# get dis vals
+qsense_offset_dis = qsense_offset_df.filter(like='D_').values[0]
 
 # convert qcmi from FWHM to actual value
-print('***OFFSETS\nQCMI:', '\n', qcmi_offset_df, '\nQSENSE:', '\n', qsense_offset_df)
+# FWHM_n / rf_n
+fwhm = qcmi_offset_df.filter(like='D_').loc[0].values # get fwhm values
+freqs = qcmi_offset_df.filter(like='F_').loc[0].values # get fwhm values
+qcmi_offset_dis = (fwhm / freqs)
+print(fwhm, freqs, qcmi_offset_dis)
+
+print('***OFFSETS\nQCMI:', '\n', qcmi_offset_dis, '\nQSENSE:', '\n', qsense_offset_dis)
 
 
 qsense_cols = [
@@ -38,13 +43,39 @@ qcmi_cols = [
     'Channel A 13. Dissipation Variation [ ]'
 ]
 
+qcmi_val_cols = [
+    'Channel A QCM Time [sec]',
+    #'Channel A Fundamental Dissipation [ ]',
+    'Channel A 3. Dissipation  [ ]',
+    'Channel A 5. Dissipation  [ ]',
+    'Channel A 7. Dissipation  [ ]',
+    'Channel A 9. Dissipation  [ ]',
+    'Channel A 11. Dissipation  [ ]',
+    'Channel A 13. Dissipation  [ ]',
+
+]
+
 dis_fig, dis_ax = plt.subplots()
 dis_fig.suptitle("Combined Dissipation")
 qcmi_fig, qcmi_ax = plt.subplots()
 qcmi_fig.suptitle("QCMI Dissipation")
 qsense_fig, qsense_ax = plt.subplots()
 qsense_fig.suptitle("Qsense Dissipation")
+qcmi_val_fig, qcmi_val_ax = plt.subplots()
 num_overtones = len(qcmi_cols)
+
+# adding offset vals
+for i, col in enumerate(qsense_cols):
+    if not col.__contains__('Time'):
+        print(qsense_offset_dis[i-1])
+        qsense_df[col] += qsense_offset_dis[i-1]
+
+for i, col in enumerate(qcmi_cols):
+    if not col.__contains__('Time'):
+        qcmi_df[col] += qcmi_offset_dis[i-1]
+        
+print("***OFFSETS ADDED\n", qcmi_df, '\n', qsense_df)
+
 
 for i in range(num_overtones-1):
     qcmi_ax.plot(qcmi_df[qcmi_cols[0]].values, qcmi_df[qcmi_cols[i+1]],'o', markersize=2, label=qcmi_cols[i+1], markerfacecolor='none', markeredgewidth=0.32)
